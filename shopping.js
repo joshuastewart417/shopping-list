df96635df6a686cd28812ac3b30cb15b730e9552
joshuastewart417 <joshua.stewart780@gmail.com>
2021-06-02 09:26:40 -0500

    initial commit

---
+++ shopping.js
@@ -0,0 +1,40 @@
+const list = [
+{
+    id:1,
+    foodType:"milk",
+    price:2,
+},
+{
+    id:2,
+    foodType:"eggs",
+    price:3,
+},
+{
+    id:3,
+    foodType:"pizza rolls",
+    price:6,
+},
+{
+    id:4,
+    foodType:"chicken",
+    price:8,
+},
+{
+    id:5,
+    foodType:"pineapple",
+    price:3,
+},
+{
+    id:6,
+    foodType:"Sandwich buns",
+    price:2.50,
+},
+{
+    id:7,
+    foodType:"mustard",
+    price:3.50,
+}
+
+]
+
+console.log(list);
